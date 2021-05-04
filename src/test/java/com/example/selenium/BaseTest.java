94f385c5e509b5a6f55df430684b6778a50f80e4
anastasiia-titarenko1 <titnastya92@gmail.com>
2021-05-04 21:07:26 +0300

    hw_14

--- src/test/java/com/example/selenium/BaseTest.java
+++ src/test/java/com/example/selenium/BaseTest.java
@@ -1,9 +1,13 @@
 package com.example.selenium;
 
 import com.example.selenium.steps.UserSteps;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     protected WebDriver driver;
@@ -11,11 +15,15 @@ public class BaseTest {
 
     @BeforeClass
     public void setUp() {
-        // TODO initialize a driver, open login URL, resize windows, and initialize steps
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/login");
+        driver.manage().window().setSize(new Dimension(840, 1027));
+        userSteps = new UserSteps(driver);
     }
 
     @AfterClass
     public void tearDown() {
-        // TODO driver close and quit
+        driver.close();
+        driver.quit();
     }
 }
